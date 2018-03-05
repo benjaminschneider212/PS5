29983035e2a3361f7afa06e3b6e402a38e33e92f
Benjamin Schneider <benjamin.schneider@wustl.edu>
2018-03-04 19:25:39 -0600

    Change all the nonsense
    fixed my function,  tweaked the generic stuff

--- Projectbones.R
+++ Projectbones.R
@@ -20,43 +20,40 @@ setClass(Class="Simpson",
            x= c(),
            y= c())
 )
-setGeneric("integrateIt",
-           function(object="Trapezoid")  {
-             standardGeneric("intergrateIt")
-           })
-setMethod("integrateIt", "Trapezoid",
-          function(object){
-            return(object@square)
-          })
-setGeneric("integrateIt",
-           function(object="Simpson")  {
-             standardGeneric("intergrateIt")
-           })
-setMethod("integrateIt", "Simpson",
-          function(object){
-            return(object@square)
-          })
 
-#dummy function
-integrateIt()<-function(x,y,type=NULL){
-  a<-min(x)
-  b<-max(x)
-  h<-(b-a)/2
-  if(type=="Trapezoid")(
-    
-    trapvalue<-(h/2)*sum()
-    return(trapvalue)
-  )
-  if(type=="Simpson")(
-    simpvalue<-(h/3)*sum()
-    return(simpvalue)
-  )
-  if(type=="both")(
-    
-    return(value)
-  )
-  if(is.null(type))(
-    print("You done messed up, pick a type")
-  )
-  else(print("uh oh boo, you messed up, pick a valid type"))
-}
+setGeneric(name="integrateIt",
+           def=function(x, y, bounds, rule)
+           {standardGeneric("integrateIt")}
+)
+
+setMethod(f="integrateIt",
+          definition=function(x,y,startandend=c(a,b),Rule, ...){
+            x<-as.matrix(x)
+            x<-t(x)
+            y<-as.matrix(y)
+            y<-t(y)
+            if (Rule=="Trap"){
+              trapvec<-y*2
+              trapvec[1]<-y[1]
+              trapvec[ncol(y)]<-y[ncol(y)]
+              h<-(bounds[2]-bounds[1]+1)/ncol(x)
+              trapoutput<-h/2*(y_1)
+              x<-as.vector(x)
+              y<-as.vector(y)
+              return(new("Trapezoid", result=sum(trapoutput), x = x, y = y))
+            }
+            if (Rule=="Simpson"){
+              simpvec<-y
+              for (i in 1:ncol(y)){
+                if((i %% 2) == 0){
+                  simpvec[i]<-y[i]*4} 
+                else {simpvec[i]<-y[i]*2}}
+              simpvec[1]<-y[1]
+              simpvec[ncol(y)]<-y[ncol(y)]
+              h<-(bounds[2]-bounds[1]+1)/ncol(x)
+              simpoutput<-h/3*(y_1)
+              x<-as.vector(x)
+              y<-as.vector(y)
+              return(new("Simpson", result=sum(simpoutput), x = x, y = y))}
+          }
+)
